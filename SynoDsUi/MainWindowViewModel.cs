83b277c850ecbde5f21ffa48a1cab175063c946d
Pavel Spesivtsev <shellgen@gmail.com>
2014-04-14 17:19:59 +0400

    Started making a simple GUI to manage download tasks

---
+++ SynoDsUi/MainWindowViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using AutoMapper;
+using SynoDsUi.Annotations;
+using SynologyAPI;
+using SynologyRestDAL.Ds;
+
+namespace SynoDsUi
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private DownloadStation DownloadStation;
+
+        public ObservableCollection<TaskViewModel> _tasks;
+
+        public ObservableCollection<TaskViewModel> Tasks { get { return _tasks; } }
+
+        public MainWindowViewModel()
+        {
+            Mapper.CreateMap<Task, TaskViewModel>();
+            NameValueCollection appSettings = ConfigurationManager.AppSettings;
+            DownloadStation = new DownloadStation(new Uri(appSettings["host"]), appSettings["username"], appSettings["password"], CreateProxy(appSettings["proxy"]));
+
+            if (DownloadStation.Login())
+            {
+                var listResult = DownloadStation.List(String.Join(",", new []{ "detail", "transfer", "file", "tracker" }));
+                if (listResult.Success)
+                {
+                    var taskList = from task in listResult.Data.Tasks select Mapper.Map<TaskViewModel>(task);
+                    _tasks = new ObservableCollection<TaskViewModel>(taskList);
+                }
+                OnPropertyChanged("Tasks");
+                DownloadStation.Logout();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private static WebProxy CreateProxy(string proxyUrl)
+        {
+            if (String.IsNullOrWhiteSpace(proxyUrl))
+            {
+                return null;
+            }
+            return new WebProxy(new Uri(proxyUrl));
+        }
+    }
+}
---
+++ SynoDsUi/TaskViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SynologyRestDAL.Ds;
+
+namespace SynoDsUi
+{
+    public class TaskViewModel : Task
+    {
+        public string Filename { get { return Title; } }
+
+        public double Progress { get { return (long.Parse(Additional.Transfer.SizeDownloaded) / long.Parse(Size)) * 100; } }
+
+        public string DownloadedLabel { get { return string.Format("{0:F} Mb", long.Parse(Additional.Transfer.SizeDownloaded) / 1024 / 1024); } }
+        
+        public string UploadedLabel { get { return string.Format("{0:F} Mb", long.Parse(Additional.Transfer.SizeUploaded) / 1024 / 1024); } }
+
+        public string SizeLabel { get { return string.Format("{0:F} Mb", long.Parse(Size) / 1024 / 1024); } } 
+    }
+}
