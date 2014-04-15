f363e5928348c312c2893e9c77386f36a42c076f
Pavel Spesivtsev <shellgen@gmail.com>
2014-04-15 20:45:09 +0400

    File size formatter

---
+++ StdUtils/FileSizeUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StdUtils
+{
+    public class FileSizeUtils
+    {
+        const int scale = 1024;
+
+        public static string FormatBytes(long bytes)
+        {
+            var orders = new [] { "GB", "MB", "KB", "Bytes" };
+            var max = (long)Math.Pow(scale, orders.Length - 1);
+
+            foreach (string order in orders)
+            {
+                if (bytes > max)
+                    return string.Format("{0:##.##} {1}", decimal.Divide(bytes, max), order);
+
+                max /= scale;
+            }
+            return "0 Bytes";
+        }
+    }
+}
--- SynoDsUi/TaskViewModel.cs
+++ SynoDsUi/TaskViewModel.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
+using StdUtils;
+
 using SynologyRestDAL.Ds;
 
 namespace SynoDsUi
@@ -12,11 +15,11 @@ namespace SynoDsUi
 
         public double Progress { get { return long.Parse(Size) == 0 ? 0 : (long.Parse(Additional.Transfer.SizeDownloaded) / long.Parse(Size)) * 100; } }
 
-        public string DownloadedLabel { get { return string.Format("{0:F} Mb", long.Parse(Additional.Transfer.SizeDownloaded) / 1024 / 1024); } }
+        public string DownloadedLabel { get { return FileSizeUtils.FormatBytes(long.Parse(Additional.Transfer.SizeDownloaded) / 1024 / 1024); } }
         
-        public string UploadedLabel { get { return string.Format("{0:F} Mb", long.Parse(Additional.Transfer.SizeUploaded) / 1024 / 1024); } }
+        public string UploadedLabel { get { return FileSizeUtils.FormatBytes(long.Parse(Additional.Transfer.SizeUploaded) / 1024 / 1024); } }
 
-        public string SizeLabel { get { return string.Format("{0:F} Mb", long.Parse(Size) / 1024 / 1024); } }
+        public string SizeLabel { get { return FileSizeUtils.FormatBytes(long.Parse(Size) / 1024 / 1024); } }
 
         public string CreatedLabel { get { return Additional.Detail.CreateTime == null ? "" : String.Format("{0:U}", DateTime.Parse (Additional.Detail.CreateTime)); } }
     }
